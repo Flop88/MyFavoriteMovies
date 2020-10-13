0f334699b9c32dec36f47ab45e57d5ea6e3fb39a
Flop88 <wwwqwwwqwwwqwwwq@gmail.com>
2020-10-13 10:06:30 +0300

    Bug fix, app work.

--- app/src/main/java/ru/mvlikhachev/myfavoritemoovies/MainActivity.java
+++ app/src/main/java/ru/mvlikhachev/myfavoritemoovies/MainActivity.java
@@ -7,13 +7,24 @@ import com.google.android.material.snackbar.Snackbar;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
+import androidx.lifecycle.Observer;
+import androidx.lifecycle.ViewModelProvider;
 
+import android.util.Log;
 import android.view.View;
 
 import android.view.Menu;
 import android.view.MenuItem;
 
+import java.util.List;
+
+import ru.mvlikhachev.myfavoritemoovies.Model.Genre;
+import ru.mvlikhachev.myfavoritemoovies.Model.Movie;
+import ru.mvlikhachev.myfavoritemoovies.ViewModel.MainActivityViewModel;
+
 public class MainActivity extends AppCompatActivity {
+
+    private MainActivityViewModel mainActivityViewModel;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,6 +32,29 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
+        mainActivityViewModel = new ViewModelProvider
+                .AndroidViewModelFactory(getApplication())
+                .create(MainActivityViewModel.class);
+
+        mainActivityViewModel.getGenres().observe(this, new Observer<List<Genre>>() {
+            @Override
+            public void onChanged(List<Genre> genres) {
+                for (Genre genre : genres) {
+                    Log.d("TAG", "Genre" + genre.getGenreName());
+                }
+            }
+        });
+
+        mainActivityViewModel.getMovies(2).observe(this, new Observer<List<Movie>>() {
+            @Override
+            public void onChanged(List<Movie> movies) {
+
+                for (Movie movie : movies) {
+                    Log.d("TAG", "Movie:" + movie.getMovieName());
+                }
+            }
+        });
 
         FloatingActionButton fab = findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
